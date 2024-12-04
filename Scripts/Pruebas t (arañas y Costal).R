af4085daa85e42d40986a9f9b4bb1132a62254c7
Gera117 <agustinvalladares2000@gmail.com>
2024-12-04 15:13:00 -0600

    12 de Noviembre del 2024

---
+++ Scripts/Pruebas t (arañas y Costal).R
@@ -0,0 +1,52 @@
+#prueba de pérsonas que le tiene miedo a las arañas
+
+Grupo <- gl(2, 12, labels = c("Fotografia", "Araña"))
+Ansiedad <- c(30, 35, 45, 40, 50, 35, 55, 25, 30, 45, 40, 50, 40, 35, 50, 55, 65, 55, 50, 35, 30, 50, 60, 39)
+
+Datos <- data.frame(Grupo, Ansiedad)
+
+head(Datos)
+
+tapply(Datos$Ansiedad, Datos$Grupo, mean)
+boxplot(Datos$Ansiedad ~ Datos$Grupo,
+        col = "red")
+plot(density(Datos$Ansiedad),
+     col = "purple")
+
+hist(Datos$Ansiedad, 
+     col = "blue")
+
+t.test(Datos$Ansiedad ~ Datos$Grupo, var.equal = T)
+
+t.test(Datos$Ansiedad ~ Datos$Grupo, var.equal = T,
+       alternative = "greater")
+
+#Ejercicio 2
+
+Costal <- c(87.7, 80.1, 77.28, 78.76, 81.52, 74.2, 80.71, 79.5, 77.87, 81.94, 80.7,
+            82.32, 75.78, 80.19, 83.91, 79.4, 77.52, 77.62, 81.4, 74.89, 82.95,
+            73.59, 77.92, 77.18, 79.83, 81.23, 79.28, 78.44, 79.01, 80.47, 76.23, 
+            78.89, 77.14, 69.94, 78.54, 79.7, 82.45, 77.29, 75.52, 77.21, 75.99, 
+            81.94, 80.41, 77.7)
+
+plot(density(Costal),
+main = "Producto costal",
+xlab = "Peso (kg) costales",
+ylab = "Densidad",
+col = "red",
+lwd = 2)
+abline(v=mean(Costal), col = "black", lwd =2, lty = 2)
+abline(v = 80, col = "yellow", lwd = 2, ity = 2)
+
+t.test(Costal, mu = 80, alternative = "less")
+
+
+
+
+
+
+
+
+
+
+
